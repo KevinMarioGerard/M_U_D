d6d146aebbfd7d0531122553fd246d0c788c34ef
Kevin Mario Gerard <kevinmariogerard@gmail.com>
2019-02-27 22:09:12 +0530

    Updating the anchor for nav bar title element

--- herokuServer/webapp/app.py
+++ herokuServer/webapp/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__,  template_folder='reactApp', static_folder='reactApp/static')
 
+@app.route("/index/")
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
