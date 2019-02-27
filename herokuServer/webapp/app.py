1ab4fa72a654f7b298f5f896fffea69a816bc9c3
Kevin Mario Gerard <kevinmariogerard@gmail.com>
2019-02-27 22:22:55 +0530

    Updated flask removal of app.run('index')

--- herokuServer/webapp/app.py
+++ herokuServer/webapp/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__,  template_folder='reactApp', static_folder='reactApp/static')
 
-@app.route("/index/")
 @app.route("/")
 def index():
     return render_template('index.html')
