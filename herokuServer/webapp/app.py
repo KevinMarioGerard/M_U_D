a73140962f390a5eee02b71211ef2d402cd00bae
Kevin Mario Gerard <30295168+KevinMarioGerard@users.noreply.github.com>
2019-02-27 18:17:18 +0530

    React static page
    React base template for home webpage

--- herokuServer/webapp/app.py
+++ herokuServer/webapp/app.py
@@ -1,30 +1,9 @@
-import os
+from flask import Flask, render_template
+app = Flask(__name__,  template_folder='reactApp', static_folder='reactApp/static')
 
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Kevin, Mario, Gerard, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello world!'
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run()
