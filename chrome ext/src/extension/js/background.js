f39a7f55f824fa75de070a081a517b89ddfad00a
Kevin Mario Gerard <kevinmariogerard@gmail.com>
2019-03-03 20:38:24 +0530

    Cleaning branch

--- chrome ext/src/extension/js/background.js
+++
@@ -1,6 +0,0 @@
-chrome.webNavigation.onCompleted.addListener(function() {
-    chrome.tabs.executeScript( null, {file: 'js/scan.js'});
-}, {url: [{urlMatches : 'https://*/'}]});
-
-chrome.contextMenus.create({"title": "Scan URL...", "contexts":["link"], "id": "scanningURL"});
-chrome.contextMenus.create({"title": "Scan Page...", "contexts":["page"], "id": "scanningPage"});
--- chrome ext/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Popup from "./containers/Popup/Popup";
-
-ReactDOM.render(<Popup />, document.getElementById("root"));
