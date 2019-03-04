94eed181c57193e5cccb4ee33c01c8b30d11311e
Kevin Mario Gerard <30295168+KevinMarioGerard@users.noreply.github.com>
2019-03-04 12:38:17 +0530

    added path for get request
    /todo/api/v1/url

--- herokuServer/webapp/app.py
+++ herokuServer/webapp/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__,  template_folder='reactApp', static_folder='reactApp/static')
 
+sample = {
+    'title': u'M.U.D.',
+    'description': 'Classify between benign and malicious urls'
+}
+
+@app.route('/todo/api/v1/url', methods=['GET'])
+def get_tasks():
+    return jsonify({'response': sample})
+
 @app.route("/")
 def index():
     return render_template('index.html')
